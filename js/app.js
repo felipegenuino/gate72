5b6eeaebfe1dc432416ef15c264969fb3bc43f2e
Felipe Genuino <design@felipegenuino.com>
2014-09-28 04:40:49 -0300

    Update slider

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+
+$(document).ready(function(){
+  $(".owl-carousel-component").owlCarousel({
+ //  	 animateOut: 'fadeOutLeft',
+	// animateIn: 'fadeInRight',
+	center:true,
+	items:1,
+	smartSpeed:450,
+	nav:true,
+	loop:true,
+	autoplay:true,
+	autoplayHoverPause:true
+     });
+});
+
+ 
